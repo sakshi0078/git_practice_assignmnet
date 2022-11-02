b8c682551fa861bdf002cf479c2674efc646776f
sakshi0078 <sj7301826@gmail.com>
2022-11-02 13:26:51 +0530

    created primeproblem

---
+++ primeproblem.js
@@ -0,0 +1,19 @@
+function checkPrime(num){
+let count=0;
+for(let i=1; i<=num; i++){
+if(num%2==0){
+count++ 
+ }
+  }
+if(count==2){ 
+return true;
+}
+return false;
+ }
+
+let ans=checkPrime(13)
+if(ans!=true){
+console.log("prime")
+}else{ 
+console.log("not prime") 
+}
